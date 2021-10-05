480157b9c5d59ab7cf70cdef18f1b1af843bc8e7
Julyellen Santos <julyellensantos1818@gmail.com>
2021-10-04 23:02:13 -0300

    Atividade 1

---
+++ Algoritimo_AumentoSalario.sql
@@ -0,0 +1,45 @@
+-----------------------ALGORITIMO AUMENTO DE SAL�RIO------------------------
+
+Begin
+	declare @salario smallmoney, @novoSalario smallmoney, @reajuste int
+
+	set @salario = 400.00
+
+	If @salario >= 0 and @salario <= 400.00
+	Begin
+		set @reajuste = 15
+		set @novoSalario = @salario * @reajuste / 100 + @salario
+		print 'Novo salario: ' + cast(@novoSalario as varchar(20))
+	End
+	Else if @salario >= 400.01 and @salario <= 800
+	Begin
+		set @reajuste = 12
+		set @novoSalario = @salario * @reajuste / 100 + @salario
+		print 'Novo salario: ' + cast(@novoSalario as varchar(20))
+	End
+	Else if @salario >= 800.01 and @salario <= 1200.00
+	Begin
+		set @reajuste = 10
+		set @novoSalario = @salario * @reajuste / 100 + @salario
+		print 'Novo salario: ' + cast(@novoSalario as varchar(20))
+	End
+	Else if @salario >= 1200.01 and @salario <= 2000.00
+	Begin
+		set @reajuste = 7
+		set @novoSalario = @salario * @reajuste / 100 + @salario
+		print 'Novo salario: ' + cast(@novoSalario as varchar(20))
+	End
+	Else if @salario > 2000.00
+	Begin
+		set @reajuste = 4
+		set @novoSalario = @salario * @reajuste / 100 + @salario
+		print 'Novo salario: ' + cast(@novoSalario as varchar(20))
+	End
+	Else
+	Begin
+		print 'intervalo nao testado'
+	End
+	print 'Reajuste ganho: ' + cast(@novoSalario - @salario as varchar (20))
+	print 'Em percentual: ' + cast(@reajuste as varchar(20)) + '%'
+End
+Go
